b889d36eb2d7b0d6499bbdb33807d87f01c72bac
Emma Gale <emmajgale@hotmail.com>
2024-04-09 09:09:44 +0100

    add in script file to upload data to the database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,46 @@
+// this is a script file which can be used to write data from the items.json file to the database if required
+const mongoose = require('mongoose');
+const fs = require('fs');
+const config = require('../../utils/config');
+
+const Item = require('../../models/item');
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => console.log('connected to MongoDB'))
+  .catch(err => {
+    console.log('error connectiong to MongoDB', err.message);
+  });
+
+// READ JSON File (and convert into a javascript object)
+const items = JSON.parse(fs.readFileSync(`${__dirname}/items.json`, 'utf-8'));
+
+// Import data into database
+const importData = async () => {
+  try {
+    await Item.create(items);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE all data that is currently in the Database
+const deleteData = async () => {
+  try {
+    await Item.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+console.log(process.argv);
--- models/item.js
+++ models/item.js
@@ -1,37 +1,45 @@
 const mongoose = require('mongoose');
 
-const itemSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: [true, 'Item MUST have a title!!!']
-    },
-    // img: {
-    //   type: String,
-    //   required: true
-    // },
-    category: {
-      type: String,
-      required: [true, 'Please specify a category']
-    },
-    price: {
-      type: Number,
-      required: [true, 'Please specify a price']
-    },
-    desc: {
-      type: String,
-      required: [true, 'Please describe the item']
-    },
-    size: {
-      type: String,
-      required: [true, 'Please specify a size']
-    },
-    onSale: {
-      type: Boolean,
-      default: false
-    }
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Item MUST have a title!!!'],
+    trim: true
   },
-  { timestamps: true }
-);
+  img: {
+    type: String,
+    required: true
+  },
+
+  // if there are multiple images
+  // images: [String]
+
+  category: {
+    type: String,
+    required: [true, 'Please specify a category']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please specify a price']
+  },
+  desc: {
+    type: String,
+    required: [true, 'Please describe the item'],
+    trim: true
+  },
+  size: {
+    type: String,
+    required: [true, 'Please specify a size']
+  },
+  onSale: {
+    type: Boolean,
+    default: false
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
 
 module.exports = mongoose.model('Item', itemSchema);
