79c4102c8cc2caa78feb4c73933d2f0e1488c582
Emma Gale <emmajgale@hotmail.com>
2024-04-11 09:53:45 +0100

    start routes to pages

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-// establish connection to database
+const path = require('path');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const express = require('express');
 const config = require('./utils/config');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// route to static files
+app.use(express.static(path.join(`${__dirname}/public`)));
 
 const itemRouter = require('./routes/itemRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -27,15 +33,41 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-// route to static files
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+const items = [
+  {
+    title: 'long coat',
+    img: '#',
+    category: 'coats',
+    price: 355,
+    desc: 'long white coat',
+    size: '10'
+  },
+  {
+    title: 'applique dress',
+    img: '#',
+    category: 'dresses',
+    price: 455,
+    desc: 'long white organza dress wit applique',
+    size: '12'
+  }
+];
+
+loggedIn = true;
 // the routers are also middleware to be added to these specific routes
+app.get('/', (req, res) => {
+  res.status(200).render('index', { loggedIn: loggedIn, items: items });
+});
+app.get('/sell', (req, res) => {
+  res.status(200).render('sell', { loggedIn: loggedIn, errors: null });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about');
+});
 app.use('/api/v1/items', itemRouter);
 app.use('/api/v1/users', userRouter);
 
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -39,7 +39,7 @@ const sendErrorProd = (err, res) => {
     // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
-    console.error('ERROR 💥', err);
+    console.error('ERROR 😫🤓', err);
 
     // 2) Send generic message
     res.status(500).json({
